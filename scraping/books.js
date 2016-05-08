b04b5134a22572d46717d0729a57c9072e879d0f
Ernesto Araiza <yo@ernestoaraiza.com>
2016-05-07 23:26:40 -0500

    shows books over 44,000, script books.js gets image thumgs

--- scraping/books.js
+++ scraping/books.js
@@ -2,7 +2,7 @@ var request = require("request"),
   cheerio = require("cheerio"),
   p;
   
-for (p=1; p<=445; p++){
+for (p=1; p<=1; p++){
   //var url = "https://www.goodreads.com/list/show/10198.Books_With_a_Goodreads_Average_Rating_of_Over_4_5?page="+p;
   //var url = "https://www.goodreads.com/list/show/75146.Books_With_a_Goodreads_Average_Rating_of_4_and_Above_?page="+p;
   var url = "https://www.goodreads.com/list/show/1.Best_Books_Ever?page="+p;
@@ -12,7 +12,8 @@ for (p=1; p<=445; p++){
           titles = [],
           authors = [],
           avgrating = [],
-          numratings = [];
+          numratings = [], 
+          img=[];
 
         console.log("********* page " + $(".pagination").children(".current").text() + " ************")
         $(".bookTitle").children("span[itemprop='name']").each(function(i, elem) {
@@ -31,9 +32,13 @@ for (p=1; p<=445; p++){
           numratings[i] = split[1]
           //console.log(avgrating[i] + " - " + numratings[i]);
         });
+        $(".bookSmallImg").each(function(i, elem) {
+          img[i] = $(this).attr('src');
+          //console.log(authors[i]);
+        });
 
         for (var i=0; i<=99; i++) //number of books per page
-          console.log(titles[i]+"\t"+authors[i]+"\t"+avgrating[i]+"\t"+numratings[i]);
+          console.log(titles[i]+"\t"+authors[i]+"\t"+avgrating[i]+"\t"+numratings[i]+"\t"+img[i]);
 
     } else {
       console.log("We’ve encountered an error: " + error);
